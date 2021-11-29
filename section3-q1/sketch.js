1a157cc1478ac781aecbe69ebfd3d67111b98103
Mako Ichikawa <2020518h@stu.kobe-u.ac.jp>
2021-11-29 21:43:45 +0900

    Update sketch.js
    q

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -11,6 +11,17 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
+  console.log(count);
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+//if(count >40 && cont <60)
+
+if(count <50){ellipse(width / 2, height / 2, count);}
+else{ellipse(width / 2, height / 2, 100-count);}
+//  let size= count;
+  //ellipse(width / 2, height / 2, size);
+
+//function draw(){
+  //background();
+//ifcount = (count + 2) % cycle;
+  //if(mouse)
 }
